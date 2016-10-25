b3ce2797996b546b53ac35f770234452ef8954f1
梁健 <liangjian@j1.com>
2016-10-25 11:49:15 +0800

    initial

---
+++ LJDownloadManager/LJDownloadManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJDownloadManager
+//
+//  Created by liang on 16/10/25.
+//  Copyright © 2016年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LJDownloadManager/LJDownloadManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LJDownloadManager
+//
+//  Created by liang on 16/10/25.
+//  Copyright © 2016年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
